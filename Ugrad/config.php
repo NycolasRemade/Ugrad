7e1f717b69ddd3d995e8b9d81a7e992ac9aab56d
NycolasRemade <nycolasmoraespierobon@gmail.com>
2026-05-28 12:24:55 -0300

    A IA faz ruim e eu faço bão

--- Ugrad/config.php
+++ Ugrad/config.php
@@ -1,14 +1,14 @@
 <?php
-$host    = 'localhost';
-$db      = 'Ugrad';
-$user    = 'root';
-$pass    = '';
+$host = 'localhost';
+$db   = 'Ugrad';
+$user = 'root';
+$pass = '';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=utf8";
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Ativa notificações de erros estruturais
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // Retorna os dados como array associativo
-    PDO::ATTR_EMULATE_PREPARES   => false,                  // Desativa emulação para maior segurança contra SQL Injection
+     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
+     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       
+     PDO::ATTR_EMULATE_PREPARES   => false,                  
 ];
 
 try {

66fdf4d39665313b7947746e5559d3e782787f30
NycolasRemade <nycolasmoraespierobon@gmail.com>
2026-09-04 07:43:22 -0300

    commit de 45 minutos

--- pesquisa_de_projetos.php
+++ pesquisa_de_projetos.php
@@ -6,7 +6,14 @@ $pdp = true;
 include 'header.php'
 ?>
 
+<div id='title_paper'>
+    <h1 class='meringue'>Pesquisa de Projetos</h1>
+    <img src="Fotos/Polygon 3.png" alt="title">
+</div>
 
+<div id='main_paper'>
+
+</div>
 
 </body>
 </html>

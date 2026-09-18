64634f244f5d55913df730bbbe53f25b3d0dd33f
NycolasRemade <nycolasmoraespierobon@gmail.com>
2026-09-18 08:29:18 -0300

    tentando pensar em uma maneira de visualizar pro- jetos aleatoriamente sem carregar todos ao mesmo tempo

--- pesquisa_de_projetos.php
+++ pesquisa_de_projetos.php
@@ -1,4 +1,32 @@
 <?php
+session_start();
+require_once 'Servidor/config.php';
+
+if (!isset($_SESSION['usuario_id'])) {
+    header('Location: login.php');
+    exit;
+}
+
+$projetos_qtd = $pdo->query(
+    'SELECT COUNT(*) from projetos
+    WHERE estado = 3'
+)->fetch();
+
+$projetos_id = array();
+
+
+for ($i=0; $i <= 20; $i++) { 
+    $p = rand(1,$projetos_qtd);
+    $projetos_id[$i] = $p;
+}
+
+
+
+
+
+$usuario_id = $_SESSION['usuario_id'];
+$usuario_id_instituicao = $_SESSION['usuario_id_instituicao'] ?? null;
+
 //////////////////////////////////
 $title = 'Pesquisa de Projetos';
 $href = 'dashboard.php';
@@ -14,6 +42,12 @@ include 'header.php'
 </div>
 
 <div id='main_paper'>
+
+    <?php if (!empty($erro)): ?>
+        <p style="color: red; padding: 0 20px;"><strong><?= htmlspecialchars($erro) ?></strong></p>
+    <?php endif; ?>
+
+    
 
 </div>
 

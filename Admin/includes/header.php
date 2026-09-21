256f4247e6faf4b90800e5f97211af747adfd51c
NycolasRemade <nycolasmoraespierobon@gmail.com>
2026-09-21 12:07:52 -0300

    Lá vamos nós...

--- Admin/includes/header.php
+++ Admin/includes/header.php
@@ -8,9 +8,6 @@ $pageTitle = $pageTitle ?? 'Admin';
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?= htmlspecialchars($pageTitle) ?> · Ugrad Admin</title>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500;600;700&display=swap" rel="stylesheet">
 <?php $cssPath = __DIR__ . '/../css/style.css'; $cssVersion = file_exists($cssPath) ? filemtime($cssPath) : time(); ?>
 <link rel="stylesheet" href="<?= isset($basePath) ? $basePath : '' ?>css/style.css?v=<?= $cssVersion ?>">
 </head>

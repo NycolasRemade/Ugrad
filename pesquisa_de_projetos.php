88060c0b1f673ed83c2e1e48af74e769c92dcf41
NycolasRemade <nycolasmoraespierobon@gmail.com>
2026-09-23 08:32:55 -0300

    quebrei mas arrumo

--- pesquisa_de_projetos.php
+++ pesquisa_de_projetos.php
@@ -71,7 +71,8 @@ include 'header.php'
         .then((data) => {
             const divProjeto = document.getElementById("info-projeto-" + id);
             divProjeto.innerHTML = 
-                "<a class='projeto-card box' href='projeto.php?id= " + id +"'Nome: " + data.nome + 
+                "<a class='projeto-card box' href='projeto.php?id= " + id +">"
+                'Nome: " + data.nome + 
                 '<br>Criado em: ' + data.data_criacao + 
                 '<br>Descrição: ' + data.descricao + 
                 '<br><img style="width: 600px; height: 200px; background-position: center; background-size: cover; background-repeat: no-repeat; background-image: url(\'data:image/jpeg;base64,' + data.img + '\');"></a>';
